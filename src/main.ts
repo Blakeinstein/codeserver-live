cbc14a39ce71922a44311682ea69d95ed1e8ad78
Blaine <rishi1998@gmail.com>
2021-07-27 19:44:18 +0530

    create layout

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
-import './style.css'
+import "flex-splitter-directive";
+import "./style.css";
+import "flex-splitter-directive/styles.min.css";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const editor = document.querySelector<HTMLDivElement>('#editor')!
+const preview = document.querySelector<HTMLDivElement>('#preview')!
+const editorUrl = 'https://codepair-meetcolab.herokuapp.com';
+const previewUrl = editorUrl + '/proxy/1234/'
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+function createIframe(src: string): HTMLIFrameElement {
+  let frame = document.createElement('iframe') as HTMLIFrameElement;
+  frame.setAttribute('src', src);
+  frame.setAttribute('allow', "clipboard-read; clipboard-write");
+  return frame;
+}
+
+function main() {
+  let editorFrame = createIframe(editorUrl);
+  editor.appendChild(editorFrame);
+
+  let previewFrame = createIframe(previewUrl)
+  preview.appendChild(previewFrame);
+  // let isPreviewFrameReady = false
+  // fetch()
+}
+window.addEventListener('load', main)
