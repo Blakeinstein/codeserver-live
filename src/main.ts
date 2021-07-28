f41458ec44122158e3618184b190e5dda2f6eacd
Blaine <rishi1998@gmail.com>
2021-07-28 18:08:29 +0530

    load iframe on first paint

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import "flex-splitter-directive/styles.min.css";
 
 const editor = document.querySelector<HTMLDivElement>('#editor')!
 const preview = document.querySelector<HTMLDivElement>('#preview')!
-const editorUrl = 'https://codepair-meetcolab.herokuapp.com';
-const previewUrl = editorUrl + '/proxy/1234/'
+const editorUrl = 'http://3.144.14.133:8080';
+const previewUrl = 'http://3.144.14.133:1234';
 
 function createIframe(src: string): HTMLIFrameElement {
   let frame = document.createElement('iframe') as HTMLIFrameElement;
@@ -14,13 +14,27 @@ function createIframe(src: string): HTMLIFrameElement {
   return frame;
 }
 
+async function checkPreviewAvailability(): Promise<Boolean> {
+  try {
+    let res = await fetch(previewUrl, {mode: 'no-cors'});
+    if (res.type == "opaque") {
+      return true;
+    }
+  } catch (err) {}
+  return false
+}
+
 function main() {
   let editorFrame = createIframe(editorUrl);
   editor.appendChild(editorFrame);
 
-  let previewFrame = createIframe(previewUrl)
-  preview.appendChild(previewFrame);
-  // let isPreviewFrameReady = false
-  // fetch()
+  
+  let interval = window.setInterval(async () => {
+    if (await checkPreviewAvailability()) {
+      let previewFrame = createIframe(previewUrl);
+      preview.appendChild(previewFrame);
+      window.clearInterval(interval);
+    }
+  }, 1000)
 }
 window.addEventListener('load', main)
